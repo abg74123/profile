8d178affebd04d4389f22e85d0b2b58c5d9218d5
kranchit precha <bisza_i.d@hotmail.com>
2023-09-23 15:15:30 +0700

    update

--- src/app/shared/work-experience/work-experience.component.ts
+++ src/app/shared/work-experience/work-experience.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, Input, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-work-experience',
@@ -7,6 +7,8 @@ import { Component, OnInit } from '@angular/core';
   ]
 })
 export class WorkExperienceComponent implements OnInit {
+
+  @Input() from = {}
 
   htmlText = `
   จัดทำ แก้ไข พัฒนาเว็บไซต์ ตาม requirements และทำตาม design ที่ได้รับมอบหมาย<br>
